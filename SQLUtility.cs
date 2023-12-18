02803fac2004d499b403415002b8fea147dd87eb
puahisrael <puahisrael1@gmail.com>
2023-12-17 20:18:31 -0600

    work session 15

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -14,13 +14,40 @@ namespace CPUFramework
             SqlConnection conn = new();
             conn.ConnectionString = ConnectionString;
             conn.Open();
+
             var cmd = new SqlCommand();
             cmd.Connection = conn;
             cmd.CommandText = sqlstatement;
             var dr = cmd.ExecuteReader();
             dt.Load(dr);
+
+            SetAllColumnsAllowNull(dt);
+
             return dt;
+        }
+
+        public static void ExecuteSQL(string sqlstatement)
+        {
+            GetDataTable(sqlstatement);
+        }
+
+        private static void SetAllColumnsAllowNull(DataTable dt)
+        {
+            foreach(DataColumn c in dt.Columns)
+            {
+                c.AllowDBNull = true;
+            }
+        }
+
+        public static void DebugPrintDataTable(DataTable dt)
+        {
+            foreach(DataRow r in dt.Rows)
+            {
+                foreach(DataColumn c in dt.Columns)
+                {
+                    Debug.Print(c.ColumnName + " = " + r[c.ColumnName].ToString());
+                }
+            }
         }
     }
 }
-//note
