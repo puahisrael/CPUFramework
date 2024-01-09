d573e29af9cf4cf31b46381b1aef14733d8650f8
puahisrael <puahisrael1@gmail.com>
2024-01-08 19:22:43 -0600

    work

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
 using System.Diagnostics;
+using System.Text;
 
 namespace CPUFramework
 {
@@ -23,12 +24,12 @@ namespace CPUFramework
 
         public static DataTable GetDataTable(SqlCommand cmd)
         {
-            Debug.Print("-----" + Environment.NewLine + cmd.CommandText);
             DataTable dt = new();
             using (SqlConnection conn = new(SQLUtility.ConnectionString))
             {
                 conn.Open();
                 cmd.Connection = conn;
+                Debug.Print(GetSQL(cmd));
                 SqlDataReader dr = cmd.ExecuteReader();
                 dt.Load(dr);
             }
@@ -49,6 +50,7 @@ namespace CPUFramework
         public static int GetFirstColumnFirstRowValue(string sql)
         {
             int n = 0;
+
             DataTable dt = GetDataTable(sql);
             if(dt.Rows.Count > 0 && dt.Columns.Count > 0)
             {
@@ -66,6 +68,49 @@ namespace CPUFramework
             {
                 c.AllowDBNull = true;
             }
+        }
+
+        public static string GetSQL(SqlCommand cmd)
+        {
+            string val = "";
+#if DEBUG
+            StringBuilder sb = new();
+
+            if(cmd.Connection != null)
+            {
+                sb.AppendLine($"--{cmd.Connection.DataSource}");
+                sb.AppendLine($"use {cmd.Connection.Database}");
+                sb.AppendLine("go");
+            }
+
+            if (cmd.CommandType == CommandType.StoredProcedure) 
+            {
+                sb.AppendLine($"exec {cmd.CommandText}");
+                int paramcount = cmd.Parameters.Count - 1;
+                int paramnum = 0;
+                string comma = ",";
+                foreach(SqlParameter p in cmd.Parameters)
+                {
+                    if (p.Direction != ParameterDirection.ReturnValue)
+                    {
+                        if (paramnum == paramcount)
+                        {
+                            comma = "";
+                        }
+                        sb.AppendLine($"{p.ParameterName} = {(p.Value == null ? "null" : p.Value.ToString())}{comma}");
+                       
+                    }
+                    paramnum++;
+                }
+            }
+            else
+            {
+                sb.AppendLine(cmd.CommandText);
+            }
+
+            val = sb.ToString();
+#endif
+            return val;
         }
 
         public static void DebugPrintDataTable(DataTable dt)
